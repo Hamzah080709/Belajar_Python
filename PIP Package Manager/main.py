293019567f70c544ccf29214ba3982346f708263
unknown <rayyan.hamzah@student.alhazen.academy>
2023-11-10 17:13:57 +0700

    string color and emoji

---
+++ PIP Package Manager/main.py
@@ -0,0 +1,8 @@
+from stringcolor import *
+from emoji import *
+
+print(cs("Hamzah","cyan"))
+print(bold("Selamat datang"))
+print(cs("hamzah","blue").underline())
+print(cs("hamzah","green","white"))
+print(emojize(":smiling face with halo"))
